ae9336d04fd3a5406d3ed1a92b1d4480167f3709
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2020-11-12 13:34:21 +0100

    changed the import for time and sys

--- pancakeCalculator.py
+++ pancakeCalculator.py
@@ -1,7 +1,7 @@
 
 # the PANCAKE counter, to see how much pancakes you can make
-import time
-import sys
+from time import sleep
+from sys import exit
 
 # this measeruments are for a serving of four pancakes
 minAmountMilk = 350 #milliliter
@@ -10,47 +10,47 @@ minAmountEggs = 2
 
 # loading...
 user = input("Hello?")
-time.sleep(2)
+sleep(2)
 print("Hello " + user)
-time.sleep(2)
+sleep(2)
 print("Program loading...")
-time.sleep(5)
+sleep(5)
 print("Program done loading")
-time.sleep(2)
+sleep(2)
 
 # amount of milk (input from user)
 amountMilk = int(input("How much milk do you have (in milliliters)?"))
 print("Calculating...")
-time.sleep(2)
+sleep(2)
 
 # amount of flour (input from user)
 amountFlour = int(input("How much flour do you have (in grams)?"))
 print("Calculating...")
-time.sleep(2)
+sleep(2)
 
 # amount of eggs (input from user)
 amountEggs = int(input("How many eggs do you have?"))
 print("Calculating...")
-time.sleep(2)
+sleep(2)
 
-# checks if there is even enough to make one batch of pancakes (4)
+# checks if there is even enough to make one pancake
 if amountFlour < minAmountFlour or amountEggs < minAmountEggs or amountMilk < minAmountMilk:
-    time.sleep(5)
-    sys.exit("You can't have no pancakes today.. :(")
+    sleep(5)
+    exit("You can't have no pancakes today.. :(")
 # calculation
 else:
-    amountFlour /= minAmountFlour 
-    amountMilk /= minAmountMilk
-    amountEggs /= minAmountEggs
+    amountFlour = amountFlour / minAmountFlour 
+    amountMilk = amountMilk / minAmountMilk
+    amountEggs = amountEggs / minAmountEggs
 
 minimum = int(min(amountEggs, amountFlour, amountMilk))
 
-time.sleep(5)
+sleep(5)
 # amount of pancakes and needed materials
 print("You can make " + str(minimum*4) + " pancakes.")
-time.sleep(2)
+sleep(2)
 print("You need " + str(minimum*int(minAmountMilk)) + " milliliter milk.")
-time.sleep(2)
+sleep(2)
 print("You need " + str(minimum*int(minAmountFlour)) + " grams flour.")
-time.sleep(2)
+sleep(2)
 print("You need " + str(minimum*int(minAmountEggs)) + " eggs.")
